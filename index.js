cd6b3fb40910715e9a358a58d2f58b21f86d0d74
vetrivelanarasu <vetrivelanarasu@gmail.com>
2023-07-28 08:44:23 +0530

    fivish

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const server = jsonServer.create()
-
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
